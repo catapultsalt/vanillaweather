3b8b1d44cce0af5fb4d9d8f1d8293ed09b633b85
catapultsalt <kkwan.shei@gmail.com>
2024-05-17 20:48:50 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ let date = new Date (response.data.time *1000);
 
 let iconElement = document.querySelector("#icon");
 
-icon.innerHTML = `<img src="${response.condition.icon_url}" class="weather-app-icon" />`;
+icon.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
 
 
 cityElement.innerHTML = response.data.city;
